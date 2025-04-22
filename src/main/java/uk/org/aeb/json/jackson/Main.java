78a418439b0f0d763c34d523eeb4940d8049f853
Alan Bayley <alan.b2@outlook.com>
2025-04-22 13:11:42 +0100

    feature/add-simple-menu
    Summary
    ----
    * Update: application to output a simple menu
    
    Test
    ----
    Tested in the console

--- src/main/java/uk/org/aeb/json/jackson/Main.java
+++ src/main/java/uk/org/aeb/json/jackson/Main.java
@@ -1,7 +1,31 @@
 package uk.org.aeb.json.jackson;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
+    PrintMenu();
+
+    while (!input.equalsIgnoreCase("Q")) {
+
+      input = scanner.nextLine();
+
+      if (input.isEmpty()) {
+        PrintMenu();
+      }
+
     }
+
+  }
+  
+  private static void PrintMenu() {
+    System.out.println("Select menu option:");
+    System.out.println("-------------------");
+    System.out.println("* Display menu again (ENTER)");
+    System.out.println("* Quit (Q)");
+  }
+    
 }
